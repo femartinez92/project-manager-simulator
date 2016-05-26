b1cb16929e3a9e66ed3d3ced3c8992d0d737400f
femartinez92 <femartinez@uc.cl>
2016-05-25 23:24:04 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'spui'
+set :application, 'project-manager-simulator'
 set :repo_url, 'git@github.com:femartinez92/project-manager-simulator.git'
 
 set :use_sudo, false
